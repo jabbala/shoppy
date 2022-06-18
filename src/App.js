7c21a4192f6b33eac99b2979cd31b14a2a3e3c8f
Gunasekar Jabbala <gunasekar.ai.dev@gmail.com>
2022-06-18 21:46:52 +0530

    Sidebar Implementation

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from "./contexts/ContextProvider";
+
+
 import './App.css';
 
 const App = () => {
-  const activeMenu = false;
+  const {activeMenu} = useStateContext();
   return (
     <div>
       <BrowserRouter>
